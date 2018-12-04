4cbdc8847b9918a1b969f9ac5027b73ea2a740be
James Tobey <jctobey@eckerd.edu>
2018-12-04 14:12:50 -0600

    added some responsive code to CSS

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function($) {
+  var alterClass = function() {
+    var ww = $( window ).width();
+    if (ww <= 1000) {
+      $('.portfolio-header').addClass('is-casting-shadow is-compact');
+    }
+    else if(ww>1000){
+      $('.portfolio-header').removeClass('is-casting-shadow is-compact');
+    }
+  };
+  $(window).resize(function(){
+    alterClass();
+  });
+  //Fire it when the page first loads:
+  alterClass();
+  console.log(document.body.clientWidth)
+});
+ 
